dce7fac80ea98b977af32037ef9358c357c4116d
coderbulbul <coderbulbul@gmail.com>
2024-07-06 20:28:30 +0600

    vite.config file updated

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "dordar-backend.vercel.app",
+        target: "https://dordar-backend.vercel.app",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
