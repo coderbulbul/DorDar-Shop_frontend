2ef33779928b61bc1f4277cea11fafe9c1c7275c
coderbulbul <coderbulbul@gmail.com>
2024-07-10 17:10:20 +0600

    vite.config.js file target url changed

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://dordar-backend.vercel.app",
+        target: "http://locahost:8000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
